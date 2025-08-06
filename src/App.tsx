9589773e12a8fc5eca5198e083e22649a144ba7e
Nemanja Mitic <mitic.nemanja@hotmail.com>
2025-08-06 17:15:17 +0200

    feat: Made layout and upgrade node packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 font-inter">
+        <h1 className="text-4xl font-bold text-gray-100 mb-8 rounded-lg">Topic Word Cloud</h1>
+        <div className="fixed inset-0 bg-opacity-50 flex items-center justify-center z-50 p-4">
+          <div className="w-full bg-white rounded-lg shadow-2xl p-6 md:p-8 md:max-w-2xl animate-fade-in-up">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4 border-b pb-2"></h2>
+            <div className="space-y-2 text-gray-700">
+              <p>
+                <span className="font-semibold">Total Mentions:</span>
+              </p>
+              <p>
+                <span className="font-semibold">Positive Mentions:</span>
+              </p>
+              <p>
+                <span className="font-semibold">Neutral Mentions:</span>
+              </p>
+              <p>
+                <span className="font-semibold">Negative Mentions:</span>
+              </p>
+            </div>
+            <button className="mt-6 w-full bg-blue-800 text-white uppercase cursor-pointer py-2 px-4 rounded-md hover:bg-blue-500">
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
